c6c6a0af162dfe36432999eaa323d087c502ced4
Kroexov <kroexov@gmail.com>
2023-04-10 01:55:35 +0300

    feat: add paragraph

--- public/tgConnection.js
+++ public/tgConnection.js
@@ -2,33 +2,28 @@ window.addEventListener('load', function () {
     let tg = window.Telegram.WebApp;
     tg.MainButton.text = "Changed Text"; //изменяем текст кнопки
     tg.expand();
-    alert(tg.initData)
-    // const DetailForm = document.getElementById('DetailForm');
-    // const OrderForm = document.getElementById('OrderForm');
-    // const ContactData = document.getElementById('ContactData');
-    // const KPForm = document.getElementById('KPForm');
-    //
-    // DetailForm.classList.toggle('active');
-    //
-    // button1.addEventListener('click', () => {
-    //     tg.sendData("DetailForm")
-    // });
-    // button2.addEventListener('click', () => {
-    //     button2.classList.toggle('active');
-    //     if (button1.classList.toggle('active'))
-    //         button1.classList.toggle('active')
-    //     if (button3.classList.toggle('active'))
-    //         button3.classList.toggle('active')
-    //     chosenSymbol = button2.textContent;
-    //     UpdateAfterCurrencyChange();
-    // });
-    // button3.addEventListener('click', () => {
-    //     button3.classList.toggle('active');
-    //     if (button2.classList.toggle('active'))
-    //         button2.classList.toggle('active')
-    //     if (button1.classList.toggle('active'))
-    //         button1.classList.toggle('active')
-    //     chosenSymbol = button3.textContent;
-    //     UpdateAfterCurrencyChange();
-    // });
+
+
+    const DetailForm = document.getElementById('DetailForm');
+    const OrderForm = document.getElementById('OrderForm');
+    const ContactData = document.getElementById('ContactData');
+    const KPForm = document.getElementById('KPForm');
+    const mainText = document.getElementById('mainText');
+    
+    mainText.innerText = tg.initData
+
+    DetailForm.classList.toggle('active');
+
+    DetailForm.addEventListener('click', () => {
+        tg.sendData("DetailForm")
+    });
+    OrderForm.addEventListener('click', () => {
+        tg.sendData("OrderForm")
+    });
+    ContactData.addEventListener('click', () => {
+        tg.sendData("ContactData")
+    });
+    KPForm.addEventListener('click', () => {
+        tg.sendData("KPForm")
+    });
 }
